7dc08db90ab9c0c66d0e443911d9f625258dd666
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-11-01 10:45:37 -0400

    edit button

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function submitButtonClicked() {
          <h2>${todoName.value}</h2>
          <p>${todoDescription.value}</p>
          <button class="done-btn" id=${"done-item-" + countTodo}>Done</button>
+         <button class="edit-btn" id=${"edit-item-" + countTodo}>Edit</button>
          <button class="x-btn" id=${"delete-item-" + countTodo}>X</button>
       </li>
    `
@@ -35,8 +36,22 @@ todoList.addEventListener("click", (e)=>{
       } else if(e.target.textContent == "Active") {
          strikeThroughTodo(e.target, "", "Done")
       } else if(e.target.textContent == "X") {
-         const li = e.target.parentElement;
-         li.style.display = "none";
+         const xBtn = e.target;
+         xBtn.parentElement.remove();
+      } else if(e.target.textContent == "Edit") {
+         const editBtn = e.target;
+         const todoItem = editBtn.parentElement;
+         const editName = todoItem.children[0];
+         const editDescription = todoItem.children[1];
+         let holdValues = [editName.textContent, editDescription.textContent];
+         editName.remove();
+         editDescription.remove();
+         const newName = document.createElement("input");
+         const newDescription = document.createElement("input");
+         newName.value = holdValues[0];
+         newDescription.value = holdValues[1];
+         todoItem.insertAdjacentElement("afterBegin", newDescription);
+         todoItem.insertAdjacentElement("afterBegin", newName);
       }
    }
 });
